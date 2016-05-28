33ccd8c4836ee7661092364864b5b9ce20e2e745
Glenn Sisson <glenn.sisson@gmail.com>
2016-05-28 16:56:45 -0700

    update devenv.visual.studio.sh

--- devenv.visual.studio.sh
+++ devenv.visual.studio.sh
@@ -2,6 +2,7 @@
 . ~/.bashrc
 
 cmd='C:/Program Files (x86)/Microsoft Visual Studio 11.0/Common7/IDE/devenv.exe' #2012
+cmd='C:/Program Files (x86)/Microsoft Visual Studio 14.0/Common7/IDE/devenv.exe' #201?
 
 #nohup "$cmd" "$@" >> $HOME/nohup.out 2>&1  &
  start "$cmd" $@ &
