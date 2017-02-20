64d2de481f47a79ec72d35cbcab928fe44137646
Glenn Sisson <glenn.sisson@gmail.com>
2017-02-19 20:11:29 -0800

    mpc movie scripts

--- play.movie_00.05.25.sh
+++ play.movie_00.05.25.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-# this script will play a single video file found in the same directory as this script,
-# and at the time index indicated by the time encoded in the script's name.
+# this script (~/usr/bin/play.movie_00.05.25.sh) will play a single video
+# file found in the same directory as this script, and at the time index
+# indicated by the time encoded in the script's name.
 
 # Note: to allow double click to shart this .sh file, run the following once
 #   ~/usr/bin/assoc.sh.double.click.bat
