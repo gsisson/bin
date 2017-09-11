c5319c3d431fee42f2aea20e81238b265832b764
Glenn Sisson <glenn.sisson@gmail.com>
2017-09-10 18:45:00 -0700

    imagemagick scripts

---
+++ convert.avi.to.mp4.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+usage() {
+  echo
+  echo "USAGE: $(basename $0) -d <OUTPUT_DIRECTORY> <INPUT_AVI_FILE>..."
+  echo
+  exit 1
+}
+
+if [ $# -lt 3 -o "$1" != '-d' ]; then
+  usage
+fi
+
+shift # -d
+outdir="$1"
+shift
+
+if [ ! -d "$outdir" ]; then
+  echo
+  echo "ERROR: \"$outdir\" is not a directory!"
+  usage
+fi
+outdir=${outdir%/}
+
+for f in ${@}; do
+  case "$f" in
+    *.avi) ;;
+    *)     echo ; echo "ERROR: \"$f\" is not an AVI file!";usage;;
+  esac
+done
+
+for f in ${@}; do
+  f=${f%.avi}
+  echo + ffmpeg -deinterlace -i "$f.avi" "$outdir/$f.mp4"
+         ffmpeg -deinterlace -i "$f.avi" "$outdir/$f.mp4"
+done
