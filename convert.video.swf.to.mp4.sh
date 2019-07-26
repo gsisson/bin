9a460449097d085c477510bd517fcba9d7677ab9
Glenn Sisson <glenn.sisson@hp.com>
2019-07-25 18:45:51 -0700

    script updates

---
+++ convert.video.swf.to.mp4.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+usage() {
+  echo
+  echo "USAGE: $(basename $0) -d <OUTPUT_DIRECTORY> <INPUT_FILE>..."
+  echo
+  exit 1
+}
+
+if [ $# -lt 3 -o "$1" != '-d' ]; then
+  usage
+fi
+
+shift # -d
+outdir="$1"
+shift
+
+if [ ! -d "$outdir" ]; then
+  echo
+  echo "ERROR: \"$outdir\" is not a directory!"
+  usage
+fi
+outdir=${outdir%/}
+
+for f in "${@}"; do
+  case "$f" in
+    *.swf) ;;
+    *)     echo ; echo "ERROR: \"$f\" is not an SWF file!";usage;;
+  esac
+done
+
+for f in "${@}"; do
+  f="${f%.swf}"
+  echo + ffmpeg -deinterlace -i "$f.swf" "$outdir/$f.mp4"
+         ffmpeg -deinterlace -i "$f.swf" "$outdir/$f.mp4"
+done
--- video.rescale.HxW.sh
+++ video.rescale.HxW.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo ffmpeg -i INPUT -vf scale=WIDTHxHEIGHT,setsar=1:1 OUTPUT
+echo ffmpeg -i INPUT.mp4 -vf scale=WIDTHxHEIGHT,setsar=1:1 OUTPUT.mp4
