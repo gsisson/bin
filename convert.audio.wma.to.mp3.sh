7cd4a98c890d0976bc7e99cff18194e8db3f7b5e
Glenn Sisson <glenn.sisson@gmail.com>
2020-03-30 22:59:24 -0700

    recent updates

---
+++ convert.audio.wma.to.mp3.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo example: ffmpeg -i input.wma -acodec libmp3lame -ab 192k output.mp3
+
---
+++ subtitle.offset.sh
@@ -0,0 +1,3 @@
+echo '# ffmpeg -itsoffset OFFSET_IN_SECONDS -i input.srt output.srt'
+
+
