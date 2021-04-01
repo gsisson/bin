d9910a5197d3e221c3b854c0eea260693f7ed293
Glenn Sisson <glenn.sisson@gmail.com>
2021-04-01 12:54:59 -0700

    mac

---
+++ focus.stealer.detect.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python                                                                                                       
+from AppKit import NSWorkspace
+from datetime import datetime
+from time import sleep
+
+last_active_name = None
+while True:
+    active_app = NSWorkspace.sharedWorkspace().activeApplication()
+    if active_app['NSApplicationName'] != last_active_name:
+        last_active_name = active_app['NSApplicationName']
+        print '%s: %s [%s]' % (
+            datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            active_app['NSApplicationName'],
+            active_app['NSApplicationPath']
+        )
