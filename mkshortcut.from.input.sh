1ee4a0ca21566f52bd87ea9bd7528b8524859166
Glenn Sisson <glennaws@amazon.com>
2023-01-17 22:26:24 -0800

    update

--- mkshortcut.from.input.sh
+++ mkshortcut.from.input.sh
@@ -16,14 +16,15 @@ inshorts=false
 echo 'reading from stdin...'
 
 while read file; do
+  echo "READ $file"
   case "$file" in
     t:/*) file="/cygdrive/t/${file#t:/}";;
   esac
   echo file=:$file:
   if [ ! -f "$file" ]; then
-    #echo 1>&2
-    #echo 1>&2 "WARNING: ignoring non-file:" # (directories, probably)
-    #echo 2>&2 "  $file"
+    echo 1>&2
+    echo 1>&2 "WARNING: ignoring non-file:" # (directories, probably)
+    echo 2>&2 "  $file"
     continue
   fi
   
