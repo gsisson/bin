b0f6e51f1c9191f13557829a219b4a5769ff3c5d
Glenn Sisson <glennaws@amazon.com>
2023-04-12 14:45:55 -0700

    updates

--- git.gpr.all.sh
+++ git.gpr.all.sh
@@ -1,4 +1,4 @@
-for d in aft* ctlz* tf-* ; do
+for d in aft* ctlz* tf-* sand* sonar* terraform-* ; do
   cd $d > /dev/null 2>&1
   echo "===== $d"
   git pull --rebase
--- git.gs.all.sh
+++ git.gs.all.sh
@@ -1,4 +1,13 @@
-for d in aft* ctlz* tf-* ; do
+for d in aft* ctlz* tf-* sand* sonar* terraform-* ; do
+  cd $d > /dev/null 2>&1
+  echo "===== $d"
+  git status
+  cd - > /dev/null 2>&1
+done
+
+exit 
+
+for d in aft* ctlz* tf-* sand* sonar* terraform-* ; do
   cd $d > /dev/null 2>&1
   echo "===== $d"
   git status
